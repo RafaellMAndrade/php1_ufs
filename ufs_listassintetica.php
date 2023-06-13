c9ded687accd6fbf0661723249e28c1d763f896b
Rafaell Maurício de Souza Andrade <130567844+RafaellMAndrade@users.noreply.github.com>
2023-06-13 19:19:38 -0300

    Criando o básico da página e CSS inicial.

---
+++ ufs_listassintetica.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de UFs</title>
+    <style>
+
+    .background-image {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        opacity: 0.4;
+        pointer-events: none;
+        background-image: url('https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/1024px-Flag_of_Brazil.svg.png');
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    
+    body {
+      padding: 1em;
+      
+    }
+
+    h1 {
+      color: navy;
+    }
+
+    th,
+    td {
+      padding: 1em;
+      text-align: center;
+    }
+
+    td {
+      color: navy;
+    }
+
+    .title {
+      font-size: 4em;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="background-image">
+  </div>
+  <div>
+    <header>
+    <h1 class="title">
+        PHP #1 - Lista Sintetica de UFs    
+    <h1>
+    <p>
+      Uma lista, feita a mão, sem uso de BDs, que mostra todos os dados das 27 unidades federativas do país, a bandeira(.svg) veio do Wikimedia, comunidade irmã a Wikipedia, sendo essa, fonte das informações aqui listadas.
+    </p>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+  <div>
+</body>
+
+</html>
